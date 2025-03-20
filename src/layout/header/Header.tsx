7a2fc97df4db84b6076495fbe56f345e76d243f7
KirenaWebDev999 <elena.job.984@gmail.com>
2025-03-20 05:38:47 +0100

    add FlexWrapper

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -2,19 +2,21 @@ import styled from "styled-components";
 import {Menu} from "../../components/menu/Menu.tsx";
 import {Logo} from "../../components/logo/Logo.tsx";
 import {MenuContact} from "../../components/menu/MenuContact.tsx";
+import {FlexWrapper} from "../../components/FlexWrapper.tsx";
 
 
 export const Header = () => {
     return (
         <StyledHeader>
-            <Logo />
-            <Menu />
-            <MenuContact />
+            <FlexWrapper justifyContent="space-between">
+                <Logo />
+                <Menu />
+                <MenuContact />
+            </FlexWrapper>
         </StyledHeader>
     );
 };
 
 const StyledHeader = styled.header`
-    display: flex;
-    justify-content: space-between;
+    
 `
