fb29b473a010c4b04971022787d8ec19f873720c
KirenaWebDev999 <elena.job.984@gmail.com>
2025-03-22 15:38:51 +0100

    create Theme

---
+++ src/styles/Theme.ts
@@ -0,0 +1,41 @@
+export const theme = {
+    colors: {
+        primaryBgc: '#191919',
+        h2h3Font: '#CCCCCC', // h2, h3, textSlogan, Projects
+        linkNavFont: '#A7A7A7', // Header, Footer, textSection, placeholder, Testimony?
+        mainSectionFont: '#D9D9D9', // sectionMain
+        projectLinkFont: '#FFFFFF',
+        projectBgc: '#363636', // Project, Form
+        borderFooter: '#42446E',
+        borderForm: '#4A4A4A',
+        iconProjectColor: '#FCFCFC',
+
+        accentImg: '#E70FAA',
+        accentImg2: '#00C0FD',
+        accentFont: '#13B0F5',
+        accentFont2: '#E70FAA',
+        // TODO delete accent colors after to do gradient
+        linearGradientImg: 'linear-gradient(180deg, #E70FAA 0%, #00C0FD 100%)', // PHOTO
+        linearGradientFont: 'linear-gradient(90deg, #13B0F5 2.6%, #E70FAA 100%)', // MAIN, SLOGAN
+        linearGradientPseudoFooter: 'linear-gradient(180deg, rgba(255, 255, 255, 0.5) 0%, rgba(255, 255, 255, 0) 97.86%)',
+        linearGradientLogo: 'linear-gradient(62.8deg, #00C0FD -167.81%, #E70FAA 267.18%)',
+        // TODO add GradientColors
+        // gradient: {
+        //     linearGradientText: {
+        //         background: 'linear-gradient(180deg, #E70FAA 0%, #00C0FD 100%)',
+        //         webkitBackgroundClip: 'text',
+        //         webkitTextFillColor: 'transparent',
+        //         backgroundClip: 'text',
+        //         color: 'transparent',
+        //         display: 'inline-block',
+        //     },
+        // },
+    },
+
+media:{
+        desktop:"screen and (max-width: 1100px)",
+        tablet: "screen and (max-width: 768px)",
+        mobile: "screen and (max-width: 576px)",
+    },
+
+}
